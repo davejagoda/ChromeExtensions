84bf37ce3a916bd41ebbe7795a5b79b194024562
dj <davejagoda+github@gmail.com>
2026-05-23 05:41:10 -0700

    Add message to popup/popup.[html|js] (version 1.0.3)

--- extension-crash-course/popup/popup.js
+++ extension-crash-course/popup/popup.js
@@ -1,1 +1,7 @@
 console.log('Hello from the popup!');
+document.querySelector("#btn").addEventListener('click', () => {
+  chrome.runtime.sendMessage({ text: "Popup" });
+});
+chrome.runtime.onMessage.addListener((msg) => {
+  document.body.innerHTML += `<div>${msg.text}</div>`;
+});
