996806e9a9f9433d4cdc20a9667dc84d039131e4
dj <davejagoda+github@gmail.com>
2026-05-29 16:18:56 -0700

    Add more event handlers to MVX (version 0.0.7)

--- MVX/background.js
+++ MVX/background.js
@@ -1,12 +1,29 @@
 console.log("Initialized background script!");
-chrome.runtime.onInstalled.addListener((object) => {
+
+// Logs when the extension is installed
+chrome.runtime.onInstalled.addListener(() => {
   console.log("Installed background script!");
 });
-// import "./fetch-page.js";
-// console.log("Imported fetch-page.js");
-// throw new Error("foo");
+chrome.alarms.create("My alarm", { periodInMinutes: 1 });
+// Logs every minute
+chrome.alarms.onAlarm.addListener((alarmInfo) => {
+  console.log(`Alarm fired: ${alarmInfo.name}`);
+});
+// Logs when the tab state changes
+chrome.tabs.onUpdated.addListener(() => {
+  console.log("Tabs updated");
+});
+// Logs when Ctrl+Shift+J is typed
+chrome.commands.onCommand.addListener((command) => {
+  console.log(`Command: ${command}`);
+});
+// Logs when the toolbar icon is clicked
 chrome.action.onClicked.addListener(() => {
   console.log("Clicked toolbar icon!");
 });
+
+// import "./fetch-page.js";
+// console.log("Imported fetch-page.js");
+// throw new Error("foo");
 let elapsed = 0;
 setInterval(() => console.log(`${++elapsed}s`), 1000);
