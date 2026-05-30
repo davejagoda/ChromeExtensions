1e366fe63f733feee1cd124300e54d37904d8066
dj <davejagoda+github@gmail.com>
2026-05-29 17:08:10 -0700

    Add Message Hub example to MVX (version 0.0.8)

--- MVX/content-script.js
+++ MVX/content-script.js
@@ -3,3 +3,34 @@ import(chrome.runtime.getURL("fetch-page.js"));
 const el = document.createElement("script");
 el.src = chrome.runtime.getURL("fetch-page.js");
 document.body.appendChild(el);
+function initializeCountdown(currentTabId) {
+  // This will fire the runtime.onConnect event
+  // in the background
+  const port = chrome.runtime.connect({
+    name: `Tab ${currentTabId}`,
+  });
+  // Messages sent from the service worker
+  port.onMessage.addListener((msg) => {
+    console.log(port.name, msg);
+    // Keep passing the value to the background while > 0
+    if (msg.value > 0) {
+      port.postMessage({ value: msg.value });
+    }
+  });
+  // Start a countdown sequence every 1000ms
+  setInterval(() => {
+    const value = Math.floor(Math.random() * 10) + 1;
+    console.log(`New countdown sequence: ${value}`);
+    // Send the inital postMessage in the sequence
+    port.postMessage({ value });
+  }, 1000);
+}
+// Send a call/response message to the background
+// to determine current tab's ID
+chrome.runtime.sendMessage(
+  // Providing a type allows the background to filter
+  // incoming messages
+  { type: "getCurrentTabId" },
+  // Background can reply to this message with the tab ID
+  (response) => initializeCountdown(response.currentTabId)
+);
