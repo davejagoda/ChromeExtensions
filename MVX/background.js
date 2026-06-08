227713cf613478a8353de303a1c162455fbb43ec
dj <davejagoda+github@gmail.com>
2026-06-07 18:57:35 -0700

    Add register/unregister example to MVX (version 0.0.29)

--- MVX/background.js
+++ MVX/background.js
@@ -1,28 +1,21 @@
-const outerVar = "foobar";
-function wipeOutPage(bg) {
-  // Record the typeof inside the content script
-  const cs = typeof outerVar;
-  document.body.innerHTML = `${bg} -> ${cs}`;
-}
-const css = `
-body {
-    background-color: red !important;
-}`;
-chrome.action.onClicked.addListener((tab) => {
-  const target = {
-    tabId: tab.id,
-  };
-  // Record the typeof inside the background
-  const backgroundTypeof = typeof outerVar;
-  chrome.scripting.executeScript({
-    target,
-    func: wipeOutPage,
-    // This array of values will be curried
-    // into `func` (similar to Array.apply)
-    args: [backgroundTypeof]
-  });
-  chrome.scripting.insertCSS({
-    target,
-    css,
-  });
+const id = "1";
+chrome.action.onClicked.addListener(async () => {
+  const activeScripts = await chrome.scripting.getRegisteredContentScripts();
+  // Toggle the content script
+  if (activeScripts.find((x) => x.id === id)) {
+    chrome.scripting.unregisterContentScripts({
+      ids: [id],
+    });
+    console.log("Unregistered content script");
+  } else {
+    chrome.scripting.registerContentScripts([
+      {
+        id,
+        matches: ["<all_urls>"],
+        js: ["content-script.js"],
+        css: ["content-script.css"],
+      },
+    ]);
+    console.log("Registered content script");
+  }
 });
