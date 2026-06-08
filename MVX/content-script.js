45476f5149487f41193f874c97d0ec42b73936a1
dj <davejagoda+github@gmail.com>
2026-06-07 18:04:47 -0700

    Add Dynamic Script Tags example to MVX (version 0.0.25)

--- MVX/content-script.js
+++ MVX/content-script.js
@@ -1,4 +1,5 @@
 const url = chrome.runtime.getURL("foo.js");
-import(url).then((fooModule) => {
-  fooModule.bar();
-});
+const script = document.createElement("script");
+script.setAttribute("type", "module");
+script.setAttribute("src", url);
+document.head.appendChild(script);
