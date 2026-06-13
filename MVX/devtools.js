63184b2c860205798b97ed317b92229c8d9f3ea7
dj <davejagoda+github@gmail.com>
2026-06-13 07:24:19 -0700

    Add Devtools Sidebars example to MVX (version 0.0.31)

--- MVX/devtools.js
+++ MVX/devtools.js
@@ -1,4 +1,17 @@
 // Use this script to access the Devtools API
-chrome.devtools.panels.create("Demo Devtools",
-                              "",
-                              "foo_panel.html");
+chrome.devtools.panels.create(
+  "Demo Devtools",
+  "",
+  "foo_panel.html");
+chrome.devtools.panels.sources.createSidebarPane(
+  "Demo Sources Sidebar",
+  (sidebar) => {
+    sidebar.setPage("sources_sidebar.html");
+  }
+);
+chrome.devtools.panels.elements.createSidebarPane(
+  "Demo Elements Sidebar",
+  (sidebar) => {
+    sidebar.setPage("elements_sidebar.html");
+  }
+);
