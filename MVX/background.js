c264e533cc5f5e17de03573dd3c2ba8c77c7f5f5
dj <davejagoda+github@gmail.com>
2026-06-20 09:51:02 -0700

    Add listener example to MVX (version 0.0.36)

--- MVX/background.js
+++ MVX/background.js
@@ -1,21 +1,13 @@
-const id = "1";
-chrome.action.onClicked.addListener(async () => {
-  const activeScripts = await chrome.scripting.getRegisteredContentScripts();
-  // Toggle the content script
-  if (activeScripts.find((x) => x.id === id)) {
-    chrome.scripting.unregisterContentScripts({
-      ids: [id],
-    });
-    console.log("Unregistered content script");
-  } else {
-    chrome.scripting.registerContentScripts([
-      {
-        id,
-        matches: ["<all_urls>"],
-        js: ["content-script.js"],
-        css: ["content-script.css"],
-      },
-    ]);
-    console.log("Registered content script");
+let count = 0;
+function handler() {
+  console.log("Handler executed!");
+  if (++count > 4) {
+    chrome.action.onClicked.removeListener(handler);
+    console.log("Handler removed!");
   }
-});
+}
+// false
+console.log(chrome.action.onClicked.hasListener(handler));
+chrome.action.onClicked.addListener(handler);
+// true
+console.log(chrome.action.onClicked.hasListener(handler));
