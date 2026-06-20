819db6f9c38a41d2a97877f278895bb58d1b6fbc
dj <davejagoda+github@gmail.com>
2026-06-20 12:14:24 -0700

    Add Event Filtering example to MVX (version 0.0.37)

--- MVX/background.js
+++ MVX/background.js
@@ -1,13 +1,8 @@
-let count = 0;
-function handler() {
-  console.log("Handler executed!");
-  if (++count > 4) {
-    chrome.action.onClicked.removeListener(handler);
-    console.log("Handler removed!");
-  }
-}
-// false
-console.log(chrome.action.onClicked.hasListener(handler));
-chrome.action.onClicked.addListener(handler);
-// true
-console.log(chrome.action.onClicked.hasListener(handler));
+chrome.webNavigation.onCommitted.addListener((event) => {
+// browser natively performing this filtering is more performant
+  console.log("Event filtered outside of handler!");
+}, {
+  url: [
+    { hostSuffix: "wikipedia.org" }
+  ]
+});
