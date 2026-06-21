65fe8d579214d3568baca615caf0c2dcd886d9aa
dj <davejagoda+github@gmail.com>
2026-06-21 09:43:07 -0700

    Add Permission Request Idempotence to MVX (version 0.0.41)

--- MVX/options.js
+++ MVX/options.js
@@ -5,9 +5,7 @@ document.querySelector("#save").addEventListener(
   "click",
   async () => {
     chrome.permissions.getAll().then(console.log);
-    if (!(await chrome.permissions.contains(permissions))) {
-      await chrome.permissions.request(permissions);
-    }
+    await chrome.permissions.request(permissions);
     chrome.storage.sync.set({ foo: "bar" });
     chrome.permissions.getAll().then(console.log);
   });
