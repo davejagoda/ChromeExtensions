c711898445a5849a6e7538bf320ba10c28aa2fd4
dj <davejagoda+github@gmail.com>
2026-06-21 09:36:38 -0700

    Add Optional Permissions to MVX (version 0.0.40)

--- MVX/options.js
+++ MVX/options.js
@@ -1,2 +1,13 @@
-chrome.storage.sync.set({ foo: "bar" });
-chrome.permissions.getAll().then(console.log);
+const permissions = {
+  permissions: ["storage"],
+};
+document.querySelector("#save").addEventListener(
+  "click",
+  async () => {
+    chrome.permissions.getAll().then(console.log);
+    if (!(await chrome.permissions.contains(permissions))) {
+      await chrome.permissions.request(permissions);
+    }
+    chrome.storage.sync.set({ foo: "bar" });
+    chrome.permissions.getAll().then(console.log);
+  });
